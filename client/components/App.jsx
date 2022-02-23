7cbf052378ac859b0a7d4c19445bb75e02ff615b
Sam Wylie <sam.wylie20@gmail.com>
2022-02-24 11:58:34 +1300

    Added Chakra Provider and Theme

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,26 +1,22 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
 
-import { fetchFruits } from '../actions'
+// Can change these colors later
+const colors = {
+  brand: {
+    900: '#E94D3E', // STANDARD
+    800: '#C24034', // DARKER
+    700: '#F7958D', // LIGHTER
+  },
+}
+
+const theme = extendTheme({ colors })
 
 function App () {
-  const fruits = useSelector(state => state.fruits)
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(fetchFruits())
-  }, [])
-
   return (
-    <>
-      <div className='app'>
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>
-          {fruits.map(fruit => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   )
 }
 
