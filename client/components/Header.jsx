c2a4ec39c8923a35a1c1f5d889004eff2cca6b9e
angela-yen <angela.yen0210@gmail.com>
2022-02-24 15:20:32 +1300

    shows on the browser

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,53 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { PhotoPlaceholder } from 'react-placeholder-image'
+import { Box } from '@chakra-ui/react'
 
 // Create header component with logo, navlinks and New post button #4
-// Logo, Top, Jobs, Code, Events, New post 
+// Logo, Top, Jobs, Code, Events, New post
 
 function Header () {
-
-
-    return (
-        <>
-        <div>
-            <PhotoPlaceholder width={200} height={200} />
+  return (
+    <>
+      <Box>
+        <img src='./ropu-log.png'></img>
         <h1>Rōpū</h1>
-        </div>
         <nav>
-            <Link to='/'>Top</Link>
-            <Link to='/jobs'>Jobs</Link>
-            <Link to='/code'>Code</Link>
-            <Link to='/events'>Events</Link>
+          <Link to='/'>Top</Link>
+          <Link to='/jobs'>Jobs</Link>
+          <Link to='/code'>Code</Link>
+          <Link to='/events'>Events</Link>
         </nav>
         <button>New Post</button>
-        </>
-    )
+      </Box>
+    </>
+  )
 }
 
 export default Header
-
-
-
-
-
-
-// function Header () {
-//   return (
-//     <>
-//       <nav className='nav'>
-//         <Link to='/'>Shop</Link>
-//         <Link to='/cart'>Cart</Link>
-//         <Link to='/orders'>My Orders</Link>
-//       </nav>
-//       <h1>
-//         <span className='fa fa-leaf' aria-hidden='true' />
-//         {' '}Sweet As Organics{' '}
-//         <span className='fa fa-leaf' aria-hidden='true' />
-//       </h1>
-//     </>
-//   )
-// }
-
-// export default Header
-
