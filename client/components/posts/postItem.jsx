178b0655079f0c55f346a2f20e0c4a5e9e2af3eb
Ryan Mearns <ryan.j.mearns@gmail.com>
2022-02-24 14:26:00 +1300

    Add base component

--- client/components/posts/postItem.jsx
+++ client/components/posts/postItem.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
-export default function postItem() {
+import { Flex, Box, Text, Stack, HStack, Link } from '@chakra-ui/react'
+import { ArrowUpIcon } from '@chakra-ui/icons'
+import { ArrowSmUpIcon } from '@heroicons/react/solid'
+
+export default function postItem ({ index, votes, title, author, authorCohort, postCreated, commentsNum }) {
   return (
-    <div>posstItem</div>
+    <HStack spacing='8' width='full'>
+      <Box>
+        <Text fontSize='xl' fontWeight='bold'>{index}.</Text>
+      </Box>
+      <Box as='button' _focus={{
+        boxShadow:
+      '0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)'
+      }}>
+        <HStack>
+          <ArrowSmUpIcon marginTop='-1' height='32px' marginRight='1' />
+          <Text fontSize='xl' fontWeight='bold'>{votes}</Text>
+        </HStack>
+      </Box>
+      <Box width='full' border='1px' borderColor='gray.200' padding='6' borderRadius='16'>
+        <Stack>
+          <Text fontSize='lg' fontWeight='bold'>{title}</Text>
+          <Text>Posted by {author} from {authorCohort} on {postCreated} | <Link>{commentsNum}</Link> comments</Text>
+        </Stack>
+      </Box>
+    </HStack>
   )
 }
--- client/index.js
+++ client/index.js
@@ -2,13 +2,26 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
-
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
 import App from './components/App'
+
+// Can change these colors later
+const colors = {
+  brand: {
+    900: '#E94D3E', // STANDARD
+    800: '#C24034', // DARKER
+    700: '#F7958D' // LIGHTER
+  }
+}
+
+const theme = extendTheme({ colors })
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
     <Provider store={store}>
-      <App />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
     </Provider>,
     document.getElementById('app')
   )
