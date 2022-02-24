82c08a6e6e01a99360fcb7ad4e3a0bf296dd26f1
Ryan Mearns <ryan.j.mearns@gmail.com>
2022-02-24 15:05:43 +1300

    Complete component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 
-import { Box } from '@chakra-ui/react'
-
-import Post from './posts/postItem'
-
 function App () {
   return (
-    <Box padding="24">
-      <Post index='1' votes="100" title="Things you notice when you quit the news (2016)" author='Ryan' authorCohort='Harakeke' postCreated='23/2/2022'  commentsNum="12"/>
-    </Box>
+    <>
+      APP
+    </>
   )
 }
 
