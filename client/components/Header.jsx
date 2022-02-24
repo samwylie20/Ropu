8291f9f2615df5a36a090ac023924288d8741361
angela-yen <angela.yen0210@gmail.com>
2022-02-24 17:01:33 +1300

    logos and header finished

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Box, Flex, Button } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
+import { ArrowSmUpIcon, LinkIcon, OfficeBuildingIcon, CodeIcon } from '@heroicons/react/solid'
 
 import Logo from './Logo'
+import NewPost from './posts/newPost'
 
 // Create header component with logo, navlinks and New post button #4
 // Logo, Top, Jobs, Code, Events, New post
@@ -16,18 +18,22 @@ function Header () {
             <Logo />
           </Box>
           <nav>
+            < ArrowSmUpIcon />
             <Link to='/'>Top</Link>
           </nav>
           <nav>
+            <LinkIcon />
             <Link to='/jobs'>Jobs</Link>
           </nav>
           <nav>
+            < CodeIcon />
             <Link to='/code'>Code</Link>
           </nav>
           <nav>
+            <OfficeBuildingIcon />
             <Link to='/events'>Events</Link>
           </nav>
-          <Button>New Post</Button>
+          <NewPost />
         </Flex>
       </Box>
     </>
