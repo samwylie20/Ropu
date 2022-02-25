2e30b65f13696eeb66768d0a59660922390c2391
angela-yen <angela.yen0210@gmail.com>
2022-02-25 16:31:33 +1300

    fixed jobs page and finished code page/route

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import Header from './Header'
 import Home from '../pages/home'
 import Jobs from '../pages/Jobs'
+import Code from '../pages/Code'
 
 function App () {
   return (
@@ -12,6 +13,7 @@ function App () {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/jobs' element={<Jobs />} />
+        <Route path='/code' element={<Code />} />
       </Routes>
 
     </>
