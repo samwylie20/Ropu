2dac5d056bbddd319a62357d851d8cc8c028b384
Ryan Mearns <ryan.j.mearns@gmail.com>
2022-03-02 13:30:35 +1300

    Fix errors

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Stack, Spinner, Center } from '@chakra-ui/react'
 import Post from '../components/posts/postItem'
-import Comments from '../components/Comments/Comments'
 import { supabase } from '../supabaseClient'
 
 export default function Home ({ session }) {
@@ -34,34 +33,10 @@ export default function Home ({ session }) {
             return postB.post_votes - postA.post_votes
           })
             .map((post, index) => {
-              return <Post session={session} key={post.id} index={index + 1} votes={post.post_votes} title={post.post_title} author='Ryan' authorCohort='Harakeke' type='link' postCreated={post.created_at} commentsNum={post.no_comments} id={post.id}/>
-
+              return <Post id={post.id} session={session} key={post.id} index={index + 1} votes={post.post_votes} title={post.post_title} author='Ryan' authorCohort='Harakeke' type='link' postCreated={post.created_at} commentsNum={post.no_comments} id={post.id}/>
             })
-            .map((post, index) => {
-              return (
-                <Post
-                  key={post.id}
-                  index={index + 1}
-                  votes={post.post_votes}
-                  title={post.post_title}
-                  author='Ryan'
-                  authorCohort='Harakeke'
-                  type='link'
-                  postCreated={post.created_at}
-                  commentsNum={post.no_comments}
-                  id={post.id}
-                />
-              )
-            })}
-        </Stack>
-
-        <div>
-          <Comments
-            commentsUrl='http://localhost:3004/comments'
-            currentUserId='1'
-          />
-        </div>
-      </>
+        }
+      </Stack>
     )
   }
 }
