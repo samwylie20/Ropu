50cc9b1ec89faf0c50d08b27ed2db002a35a0995
Ryan Mearns <ryan.j.mearns@gmail.com>
2022-03-02 14:43:59 +1300

    Clean up

--- client/components/posts/postItem.jsx
+++ client/components/posts/postItem.jsx
@@ -27,7 +27,6 @@ export default function postItem ({ session, index, title, author, votes, type, 
       .from('upvotes')
       .select('post_id', { count: 'exact' })
       .eq('post_id', id)
-
     setCountVote(count)
   }, [])
 
@@ -57,41 +56,37 @@ export default function postItem ({ session, index, title, author, votes, type, 
       .match({ id: id })
   }
 
-  if (!countVote) {
-    return null
-  } else {
-    return (
-      <HStack spacing='6' width='full'>
-        <Box>
-          <Text fontSize='xl' fontWeight='bold'>{index}.</Text>
-        </Box>
-        <Box as='button' paddingX='4' paddingY='2' borderRadius='12' _hover={{
-          background: 'gray.50'
-        }} onClick={(e) => handleUpVote()}>
-          <HStack>
-            <ArrowSmUpIcon height='32px' style={{ marginRight: '5px', marginTop: '-2px' }} />
-            <Text fontSize='xl' fontWeight='bold'>{countVote}</Text>
-          </HStack>
-        </Box>
-        <LinkBox as='article' width='full' border='1px' borderColor='gray.200' padding='6' borderRadius='16'>
-          <HStack align='start' spacing='4'>
-            <Box marginTop='1'>
-              {type === 'link' && <LinkIcon height='24px' />}
-              {type === 'job' && <OfficeBuildingIcon height='24px' />}
-              {type === 'code' && <CodeIcon height='24px' />}
-              {type === 'events' && <CalendarIcon height='24px' />}
-            </Box>
-            <Stack>
-              <LinkOverlay href='#posturl' target="_blank">
-                <Text fontSize='lg' fontWeight='bold'>{title}</Text>
-              </LinkOverlay>
-              <Text fontSize='md' fontWeight='normal' as='i'>{description}</Text>
-              <Text fontSize='sm'>{url}</Text>
-              <Text fontSize='sm'>Posted by <Link href='#profilelink' fontWeight='bold' textColor='orange.500'>{author}</Link> from {authorCohort} on {formatDate(postCreated)} | <Link fontWeight='bold' textColor='orange.500' href='#comments'>{commentsNum} comments</Link></Text>
-            </Stack>
-          </HStack>
-        </LinkBox>
-      </HStack>
-    )
-  }
+  return (
+    <HStack spacing='6' width='full'>
+      <Box>
+        <Text fontSize='xl' fontWeight='bold'>{index}.</Text>
+      </Box>
+      <Box as='button' paddingX='4' paddingY='2' borderRadius='12' _hover={{
+        background: 'gray.50'
+      }} onClick={(e) => handleUpVote()}>
+        <HStack>
+          <ArrowSmUpIcon height='32px' style={{ marginRight: '5px', marginTop: '-2px' }} />
+          <Text fontSize='xl' fontWeight='bold'>{countVote && countVote}</Text>
+        </HStack>
+      </Box>
+      <LinkBox as='article' width='full' border='1px' borderColor='gray.200' padding='6' borderRadius='16'>
+        <HStack align='start' spacing='4'>
+          <Box marginTop='1'>
+            {type === 'link' && <LinkIcon height='24px' />}
+            {type === 'job' && <OfficeBuildingIcon height='24px' />}
+            {type === 'code' && <CodeIcon height='24px' />}
+            {type === 'events' && <CalendarIcon height='24px' />}
+          </Box>
+          <Stack>
+            <LinkOverlay href='#posturl' target="_blank">
+              <Text fontSize='lg' fontWeight='bold'>{title}</Text>
+            </LinkOverlay>
+            <Text fontSize='md' fontWeight='normal' as='i'>{description}</Text>
+            <Text fontSize='sm'>{url}</Text>
+            <Text fontSize='sm'>Posted by <Link href='#profilelink' fontWeight='bold' textColor='orange.500'>{author}</Link> from {authorCohort} on {formatDate(postCreated)} | <Link fontWeight='bold' textColor='orange.500' href='#comments'>{commentsNum} comments</Link></Text>
+          </Stack>
+        </HStack>
+      </LinkBox>
+    </HStack>
+  )
 }
